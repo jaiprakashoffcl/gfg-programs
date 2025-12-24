9feab1bae500174ddd9099f15d3131e4c3a8d1d4
Jai Prakash R <126840211+jaiprakashoffcl@users.noreply.github.com>
2025-12-24 08:54:44 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Calculator/calculator.java
@@ -0,0 +1,17 @@
+class Solution {
+    public void calculate(int a, int b, int operator) {
+        // code here
+        if(operator == 1){
+            System.out.println(a+b);
+        }
+        else if(operator == 2){
+            System.out.println(b-a);
+        }
+        else if(operator == 3){
+            System.out.println(a*b);
+        }
+        else{
+            System.out.println("Invalid Input");
+        }
+    }
+}
